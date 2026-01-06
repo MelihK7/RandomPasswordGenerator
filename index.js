e74b88cb35d4279e6d2e78280f199469c04cfc2b
Melih Kelkitli <s235114@dtu.dk>
2026-01-06 19:46:22 +0100

    Kort kodebeskrivelse

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";",
     "<",">",".","?", "/"];
 
+    // Fanger vores elementer via javascript med "document" altså DOM
     let btn1 = document.getElementById("suggest1")
     let btn2 = document.getElementById("suggest2")
 
@@ -12,50 +13,56 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     let suggest1 = document.getElementById("suggest1")
     let suggest2 = document.getElementById("suggest2")
 
+
+    // Tilknytter hver knap via javascript, og fortæller hvilken funktion den skal køre
     generateButton.addEventListener("click", function() { 
         generateRandomPassword();
     });
 
-    suggest1.addEventListener("click", function() {
-        handleCopy(suggest1);
-    });
+   suggest1.addEventListener("click", function() {
+        handleCopy(suggest1)
+   });
 
-
-    suggest2.addEventListener("click", function() {
-        handleCopy(suggest2);
-    });
-
+   suggest2.addEventListener("click",function(){
+        handleCopy(suggest2)
+   });
     
+   // Funktionen vi køre, når man trykker på endten 1. eller 2. anbefalingskode knapper.
+   // Så når man trykker, kopire vi, og når den har kopiret, overwriter vi midlertidigt textcontent
+   // i 1,5 sek. med "Copied!" og så dernæst giver TextContent med den orginale kode igen.
     function handleCopy(buttonEl) {
-        let orginialText = buttonEl.textContent;
+        let originalCodeText = buttonEl.textContent;
 
-        navigator.clipboard.writeText(orginialText).then(function() {
+        navigator.clipboard.writeText(originalCodeText).then(function() {
             buttonEl.textContent = "Copied!";
             setTimeout(function() {
-                buttonEl.textContent = orginialText;
+                buttonEl.textContent = originalCodeText;
             }, 1500);
         });
     };
 
 
     function generateRandomPassword() {
+        // Reset text content, everytime we click "Generate Password" otherwhise it will keep adding and exceed the border of the buttons
+        let password1Suggestion = "";
+        let password2Suggestion = "";
 
-        //reset text content, everytime we click "Generate Password" otherwhise it will keep adding and exceed the border of the buttons
-        btn1.textContent = "";
-        btn2.textContent = "";
-
+        // Generer forskellige numre med randomNumber
         for (let i = 0; i < 15; i++) {
-            let randomIndexBtn1 = randomNumber();
-            let randomIndexBtn2 = randomNumber();
+            password1Suggestion += characters[randomNumber()]
+            password2Suggestion += characters[randomNumber()]
+        }   
 
-
-            btn1.textContent += characters[randomIndexBtn1];
-            btn2.textContent += characters[randomIndexBtn2];
-        }
+        // Overwriter nu med vores nye fundne koder
+        suggest1.textContent = password1Suggestion;
+        suggest2.textContent = password2Suggestion;
     }
 
+    // Funktion til at give et randomt nummer med math.random,
+    // bruger math.floor for at få til nærmeste hele cifre
+
     function randomNumber() {
-        return Math.floor(Math.random() * characters.length);
+       return Math.floor(Math.random() * characters.length);
     }
 
 
